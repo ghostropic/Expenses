a7e5005e32123da8a9100883f2d517bd4837cbcb
Jon Bichar <jon.bichar@gmail.com>
2019-02-26 10:58:34 -0500

    disabled speedy for prod to render correctly

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const webpack = require('webpack');
 
 module.exports = (env) => {
   const isProduction = env === 'production'
@@ -22,13 +23,18 @@ module.exports = (env) => {
         ]
       }]
     },
-    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    plugins: [
+      new webpack.DefinePlugin({
+        SC_DISABLE_SPEEDY: true
+      })
+    ],
+    // devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       port: 3000,
       historyApiFallback: true,
       publicPath: '/dist/'
-    },
-    mode: isProduction ? 'production' : 'development'
+    }
+    // mode: isProduction ? 'production' : 'development'
   }
 }
